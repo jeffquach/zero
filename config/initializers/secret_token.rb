f4bd608a49ba0ef01b79c01595efdcc0d04827c7
David Brooks <davidbrooks.now@gmail.com>
2014-03-01 19:09:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mindmeetup::Application.config.secret_key_base = '87b9db1a1c810c15b933131a9320360d49744657a61d173c0c04ba7fea0b3964d63c1107c26227a19c7eeea1d1aff729c0a83b54a7d5995d506ac129e617c1a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mindmeetup::Application.config.session_store :cookie_store, key: '_mindmeetup_session'
