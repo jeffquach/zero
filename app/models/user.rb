33a7b881d8e3d32e14655f1bcf40a70e9905b4d3
jeffquach <jquach8@gmail.com>
2014-03-02 19:27:46 -0500

    Added validations to user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
 	authenticates_with_sorcery!
+	before_save {email.downcase!}
 	validates_confirmation_of :password
-	validates :password, presence: true, on: :create
-	validates :email, presence: true, uniqueness: true
+	validates :password, presence: true, on: :create, length: {minimum: 6}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :email, presence: true, uniqueness: {case_sensitive:false}, format: {with: VALID_EMAIL_REGEX}
+	validates :first_name, presence: true
+	validates :last_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140302203344) do
+ActiveRecord::Schema.define(version: 20140303000334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,12 @@ ActiveRecord::Schema.define(version: 20140302203344) do
     t.string   "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["activation_token"], name: "index_users_on_activation_token", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
 
