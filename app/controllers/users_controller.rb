84395bd5a90ecce3f85dc2b25cd9e3e79b22fada
jeffquach <jquach8@gmail.com>
2014-03-02 20:18:54 -0500

    Got the admin functionality working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   skip_before_filter :require_login, only: [:index,:new,:create,:activate]
+  before_filter :require_login, only: [:edit,:update,:destroy]
+  before_action :find_user, only: [:show,:edit,:update,:destroy]
+  before_filter :admin_user, only: :destroy
 
   def new
   	@user = User.new
@@ -19,6 +22,27 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:notice] = "You have updated your user information"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to users_path, alert: "You deleted a user!"
+    end
+  end
+
   def activate
     if (@user = User.load_from_activation_token(params[:id]))
       @user.activate!
@@ -30,6 +54,14 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:email,:password,:password_confirmation)
+  	params.require(:user).permit(:first_name,:last_name,:email,:password,:password_confirmation,admin: false)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+def full_title(pt)
+    base = "Base"
+    if pt.empty?
+      base
+    else
+      "#{base} | #{pt}"
+    end
+  end
 end
