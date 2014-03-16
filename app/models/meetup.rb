7d664e65002cfdfd4e94d007a4e0bc0ec5431947
jeffquach <jquach8@gmail.com>
2014-03-15 20:14:27 -0400

    Added some validations on creating a new meetup so the proper errors are thrown is a user enters incorrect data

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -8,10 +8,25 @@ class Meetup < ActiveRecord::Base
 	validates :end_time, presence: true
 	validates :number_of_people, presence: true, numericality: {only_integer: true}
 
+	validate :end_time_before_start_time
+	validate :at_least_two_people_attending_meetup
+
 	has_many :comments
 
 	def people_attending_meetup
 		invitees.map{|person| person.user.first_name}.join(", ")
+	end
+
+	def end_time_before_start_time
+		if start_time >= end_time
+			errors.add(:base, "You can't create a meetup with your chosen times!")
+		end
+	end
+
+	def at_least_two_people_attending_meetup
+		if number_of_people < 2
+			errors.add(:base, "You have to have at least 2 people at your meetup!")
+		end
 	end
 end
 
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_fit => [130, 130]
+     process :resize_to_fit => [110, 110]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
