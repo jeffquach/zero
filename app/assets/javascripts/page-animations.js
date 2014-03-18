e090b93f6a0ab5222a1cbb50aab933417deef2da
jeffquach <jquach8@gmail.com>
2014-03-17 23:17:00 -0400

    Made some minor modifications to the tooltip and some styling changes to the home page

--- app/assets/javascripts/page-animations.js
+++ app/assets/javascripts/page-animations.js
@@ -5,7 +5,26 @@
 		$('.show-bio').click(function(){
 			$('.hidden-bio').slideToggle(1);
 		});
+		runTooltip();
 	});
+
+	function runTooltip(){
+		$(document).tooltip({
+			items: '.show-bio',
+			position: { my: "left+15 top", at: "top right" },
+			content: function(callback){
+				var user_id = $("#hidden-id").attr("value");
+				$.ajax({
+					url: "/users/" + user_id + "/tooltip",
+					method: 'GET',
+					dataType: 'html',
+					success: function(data){
+						callback(data);
+					}
+				});
+			}
+		});
+	}
 
 }());
 
