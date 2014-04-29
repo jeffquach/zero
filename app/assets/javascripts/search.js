96de64d5fee7df2b3ecf80f3d04e55c40c334931
jeffquach <jquach8@gmail.com>
2014-04-29 00:18:20 -0400

    Added some minor changes to the dynamic select field

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 	$("#search-button, .user-preferences").click(function(){
 		$("#search-form, #meetup-info").slideToggle();
 	})
-
 	$("#subject_search").change(function(){
 		$.ajax({
+			method: 'GET',
 			url: "/searches/update_topics",
 			data: {subject_id: $("#subject_search").val()},
 			dataType: "script"
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,9 +3,9 @@ class SearchesController < ApplicationController
 	def update_topics
 		subject = Subject.find(params[:subject_id])
 		if subject.topics.blank?
-			@topics = [].insert(0,"No topics available")
+			@topics = []
 		else
-			@topics = subject.topics.order('lower(name) ASC').map{|t| t.name}.uniq.insert(0,"Select topic")	
+			@topics = subject.topics.order('lower(name) ASC').map{|t| t.name}.uniq	
 		end
 	end
 end
